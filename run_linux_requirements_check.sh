487da25aba9bea762b385766a02d2c05294f3cfc
mr-asa <mr.aserg@gmail.com>
2024-09-25 17:44:08 +0400

    fix launcher for checker

--- run_linux_requirements_check.sh
+++ run_linux_requirements_check.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# Change to the directory where your script is located
-cd "$(dirname "$0")"
-# Run the Python script
+
+# Get the full path of the directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Launch a new terminal window and execute the Python script
+xfce4-terminal --hold -e "bash -c '
+cd \"$SCRIPT_DIR\"
 python3 requirements_check.py
+echo \"Requirements check completed. Press any key to close this window.\"
+read -n 1
+'"
